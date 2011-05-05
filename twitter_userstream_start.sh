53bb0d5f2e5c52856ea0ce257e8307d3b884c0f3
ixixi <ixixizko@gmail.com>
2011-05-05 23:17:43 +0900

    nohup

--- twitter_userstream_start.sh
+++ twitter_userstream_start.sh
@@ -1,4 +1,4 @@
 #/usr/bin/sh
 cd src
-python twitteruserstream.py >> ../log/tw_user_stream.log 2>> ../log/tw_user_stream_err.log 
+nohup python twitteruserstream.py >> ../log/tw_user_stream.log 2>> ../log/tw_user_stream_err.log &
 #python twitteruserstream.py
